93c052d7f283c1ea450bce315e37a2d0a525bb60
이영훈 <roe961@naver.com>
2021-11-07 22:29:39 +0900

    First commit

---
+++ src/main/java/com/SpringIsConing/injagang/InjagangApplication.java
@@ -0,0 +1,17 @@
+package com.SpringIsConing.injagang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class}) //https://lemontia.tistory.com/586 참고
+public class InjagangApplication {
+
+	public static void main(String[] args) {
+		System.out.println("InjagangApplication.main~~~");
+		SpringApplication.run(InjagangApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SpringIsConing/injagang/InjagangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringIsConing.injagang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InjagangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
