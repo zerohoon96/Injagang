674d54e124bcaf0613d4fd0823fea9a6d8b6239e
SumanKim <kimsm9822@gmail.com>
2021-11-10 22:17:14 +0900

    Feat: 게시판 예상질문/피드백 추가, 자소서/면접 게시판 사이드바 메뉴 활성화 Etc: .yaml 설정에 주의

--- src/main/java/com/SpringIsComing/injagang/Controller/BoardController.java
+++ src/main/java/com/SpringIsComing/injagang/Controller/BoardController.java
@@ -3,22 +3,23 @@ package com.SpringIsComing.injagang.Controller;
 import com.SpringIsComing.injagang.DTO.PageRequestDTO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @Slf4j
 @RequestMapping("/boards")
-public class TestBoardController {
+public class BoardController {
 
     @GetMapping("/essay-list")
-    public String essayTest(PageRequestDTO pageRequestDTO){
+    public String essayTest(PageRequestDTO pageRequestDTO, Model model){
         log.info("==========TestBoardController==========");
         return "boards/essay-list";
     }
 
     @GetMapping("/interview-list")
-    public String interviewTest(PageRequestDTO pageRequestDTO) {
+    public String interviewTest(PageRequestDTO pageRequestDTO, Model model) {
         log.info("==========TestBoardController==========");
         return "boards/interview-list";
     }
