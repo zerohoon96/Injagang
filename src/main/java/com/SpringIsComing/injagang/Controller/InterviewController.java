d2ece5444a2bfcff9c1cb55f67f2f791ebe35210
zerohoon96 <roe961@naver.com>
2021-11-10 18:55:42 +0900

    Feat: 면접 설정 끝, 모의 면접 화면 기초 구성
    Feat: 면접 설정 끝, 모의 면접 화면 기초 구성

--- src/main/java/com/SpringIsComing/injagang/Controller/InterviewController.java
+++ src/main/java/com/SpringIsComing/injagang/Controller/InterviewController.java
@@ -5,6 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -32,10 +33,33 @@ public class InterviewController {
     }
 
     /**
-     * 면접 세팅 화면
+     * 모의 면접 보기 화면
      **/
-    @PostMapping("/submit")
-    void interviewSubmit() {
+    @PostMapping("/test") //이거 새로 생성한 면접 객체 PK로 바꿔야함.. (/{interviewId}) pathvariable로 밑에서 쓰면 될듯
+    String interviewSubmit(@RequestParam Map<String, Object> allParameters,
+                           Model model) {
+        List<String> questionList = new ArrayList<>();
+        //1. 인성, cs 면접 질문 디비에서 랜덤으로 뽑아서 리스트에 추가
+
+        //2. 예상 댓글 질문 디비에서 랜덤으로 뽑아서 리스트에 추가
+
+        //4. Shuffle
+        for (Map.Entry<String, Object> entry : allParameters.entrySet()) {
+            System.out.println(entry.getKey() + ":" + entry.getValue());
+        }
+        System.out.println("InterviewController.interviewSubmit");
+        questionList.add("객체지향 설계 5원칙을 설명해보세요");
+        questionList.add("본인 성격의 장단점을 말씀해주세요");
+        questionList.add("최소 신장 트리를 만드는데 사용되는 알고리즘은 어떤 것이 있나요?");
+        questionList.add("요즘 관심있는 아이티 기술 및 이유를 말해주세요");
+        questionList.add("지원 동기를 말씀해주세요");
+        model.addAttribute("questions",questionList);
+        return "/interview/interview-test";
+    }
+
+    @GetMapping("/test")
+    String test() {
+        return "/interview/test";
     }
 
 }
