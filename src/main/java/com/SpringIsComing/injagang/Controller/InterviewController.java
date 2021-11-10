2d446dde672839f27ebb6411e8d88d6e25a0460b
zerohoon96 <roe961@naver.com>
2021-11-10 14:18:23 +0900

    Feat: 면접 준비 화면 마무리
    Feat: 면접 준비 화면 마무리

--- src/main/java/com/SpringIsComing/injagang/Controller/InterviewController.java
+++ src/main/java/com/SpringIsComing/injagang/Controller/InterviewController.java
@@ -1,8 +1,15 @@
 package com.SpringIsComing.injagang.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/interview")
@@ -11,7 +18,24 @@ public class InterviewController {
      * 면접 세팅 화면
      **/
     @GetMapping("/init")
-    String interviewInit() {
+    String interviewInit(Model model) {
+        Map<String, Integer> essayMap = new HashMap<String, Integer>();
+        essayMap.put("2021 하반기 삼성전자",5);
+        essayMap.put("2021 하반기 LG전자",6);
+        essayMap.put("2021 하반기 Kakao",3);
+        essayMap.put("2021 하반기 Naver",9);
+
+        model.addAttribute("baseQuestion",10); //인성면접 개수
+        model.addAttribute("csQuestion",6); //전공면접 개수
+        model.addAttribute("essayMap",essayMap); //자소서 이름, 질문수
         return "/interview/interview-init";
     }
+
+    /**
+     * 면접 세팅 화면
+     **/
+    @PostMapping("/submit")
+    void interviewSubmit() {
+    }
+
 }
