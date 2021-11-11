f6a13f7e1152dc6013172df8fba76388795b6e73
SumanKim <kimsm9822@gmail.com>
2021-11-11 17:53:33 +0900

    Feat: 자소서/면접 게시물 틀 구성

--- src/main/java/com/SpringIsComing/injagang/Controller/BoardController.java
+++ src/main/java/com/SpringIsComing/injagang/Controller/BoardController.java
@@ -5,22 +5,40 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @Slf4j
-@RequestMapping("/boards")
+@RequestMapping
 public class BoardController {
 
-    @GetMapping("/essay-list")
-    public String essayTest(PageRequestDTO pageRequestDTO, Model model){
-        log.info("==========TestBoardController==========");
+    @GetMapping("/essay/board")
+    public String essayBoard(PageRequestDTO pageRequestDTO, Model model){
+        log.info("==========essayBoard==========");
         return "boards/essay-list";
     }
 
-    @GetMapping("/interview-list")
-    public String interviewTest(PageRequestDTO pageRequestDTO, Model model) {
-        log.info("==========TestBoardController==========");
+    @GetMapping("/interview/board")
+    public String interviewBoard(PageRequestDTO pageRequestDTO, Model model) {
+        log.info("==========interviewBoard==========");
         return "boards/interview-list";
+    }
+
+    @GetMapping("/essay/board/{eb_pk}")
+    public String essayViewer(@PathVariable Integer eb_pk,
+            PageRequestDTO pageRequestDTO, Model model) {
+        log.info("----------essayViewer----------");
+
+        /* eb_pk로 해당 Essay 객체를 찾아 DTO로 변환 후 model attribute 추가하여 넘기기 */
+        return "boards/essay-read";
+    }
+
+    @GetMapping("/interview/board/{ib_pk}")
+    public String interviewViewer(PageRequestDTO pageRequestDTO, Model model) {
+        log.info("----------interviewViewer----------");
+
+        /* ib_pk로 해당 Interview 객체를 찾아 DTO로 변환 후 model attribute 추가하여 넘기기 */
+        return "boards/interview-read";
     }
 }
