958daf06b22ee09f14fa1abc782d16bfa0afcf9d
김의현 <rladml91gus@naver.com>
2021-11-12 17:52:48 +0900

    Feat: 자소서쓰기(템플릿 결정, 직접입력)

--- src/main/java/com/SpringIsComing/injagang/Controller/EssayController.java
+++ src/main/java/com/SpringIsComing/injagang/Controller/EssayController.java
@@ -1,9 +1,15 @@
 package com.SpringIsComing.injagang.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/essay")
 public class EssayController {
+
+    @GetMapping
+    public String test() {
+        return "essay/write";
+    }
 }
