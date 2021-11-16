10dc87da88ed4c9a0fe17b23c00898a64953a7af
zerohoon96 <roe961@naver.com>
2021-11-16 23:08:56 +0900

    Feat: init 디자인 변경, init 및 test refactoring, interviewController 각 면접 개수 저장
    Feat: init 디자인 변경, init 및 test refactoring, interviewController 각 면접 개수 저장

--- src/main/java/com/SpringIsComing/injagang/Controller/InterviewController.java
+++ src/main/java/com/SpringIsComing/injagang/Controller/InterviewController.java
@@ -20,7 +20,7 @@ public class InterviewController {
      **/
     @GetMapping("/init")
     String interviewInit(Model model) {
-        Map<String, Integer> essayMap = new HashMap<String, Integer>();
+        Map<String, Integer> essayMap = new HashMap<String, Integer>(); //예상 질문이 있는 자기소개서만 추가 (면접 설정을 할때 무조건 질문 개수가 1개 이상이 되도록..)
         essayMap.put("2021 하반기 삼성전자",5);
         essayMap.put("2021 하반기 LG전자",6);
         essayMap.put("2021 하반기 Kakao",3);
@@ -39,15 +39,35 @@ public class InterviewController {
     String interviewSubmit(@RequestParam Map<String, Object> allParameters,
                            Model model) {
         List<String> questionList = new ArrayList<>();
+        int baseQuestion = Integer.parseInt(allParameters.get("baseQuestion").toString());
+        int csQuestion = Integer.parseInt(allParameters.get("csQuestion").toString());
+        int expectedQuestion = Integer.parseInt(allParameters.get("expectedQuestion").toString());
+        int userAddQuestion;
+
+        System.out.println("크기 : "+allParameters.size());
+        System.out.println(allParameters);
+
+        for(userAddQuestion=0;userAddQuestion<allParameters.size();userAddQuestion++){
+            if(!allParameters.containsKey("question"+(userAddQuestion+1))) {
+                break;
+            }
+        }
+
+        System.out.println("인성질문 개수 : "+baseQuestion);
+        System.out.println("전공질문 개수 : "+csQuestion);
+        System.out.println("예상질문 개수 : "+expectedQuestion);
+        System.out.println("추가질문 개수 : "+userAddQuestion);
+
         //1. 인성, cs 면접 질문 디비에서 랜덤으로 뽑아서 리스트에 추가
 
         //2. 예상 댓글 질문 디비에서 랜덤으로 뽑아서 리스트에 추가
 
+        //3. 직접 추가 질문 디비에서 랜덤으로 뽑아서 리스트에 추가
+
         //4. Shuffle
         System.out.println("InterviewController.interviewSubmit");
-        questionList.add("안녕하세요");
-        questionList.add("이영훈입니다");
-        questionList.add("어쩌라구요");
+        questionList.add("액션이 어떻게 되는지 아세요?");
+        questionList.add("무야호");
         model.addAttribute("questions",questionList);
         model.addAttribute("interviewName",allParameters.get("interviewName"));
         return "interview/test";
