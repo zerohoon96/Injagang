553c425d90f85e473d3a83735a00c58bda551bd6
zerohoon96 <roe961@naver.com>
2021-11-27 17:38:45 +0900

    Feat: 자기소개서 첨삭 결과 DB 저장

--- src/main/java/com/SpringIsComing/injagang/Service/FeedbackService.java
+++ src/main/java/com/SpringIsComing/injagang/Service/FeedbackService.java
@@ -2,8 +2,10 @@ package com.SpringIsComing.injagang.Service;
 
 import com.SpringIsComing.injagang.DTO.EssayFeedbackInfoDTO;
 import com.SpringIsComing.injagang.Entity.Essay;
+import com.SpringIsComing.injagang.Entity.EssayFeedback;
 import com.SpringIsComing.injagang.Entity.Member;
 
 public interface FeedbackService {
     public void storeFeedback(Member member, Essay essay, EssayFeedbackInfoDTO essayFeedbackInfoDTO);
+    public EssayFeedback findFeedback(String nickname, Long essayId);
 }
--- src/main/java/com/SpringIsComing/injagang/Service/FeedbackServiceImpl.java
+++ src/main/java/com/SpringIsComing/injagang/Service/FeedbackServiceImpl.java
@@ -8,34 +8,50 @@ import com.SpringIsComing.injagang.Entity.EssayFeedbackComment;
 import com.SpringIsComing.injagang.Entity.Member;
 import com.SpringIsComing.injagang.Repository.EssayFeedbackRepository;
 import com.SpringIsComing.injagang.Repository.EssayRepository;
+import com.SpringIsComing.injagang.Repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 @Transactional
 public class FeedbackServiceImpl implements FeedbackService {
     private final EssayFeedbackRepository essayFeedbackRepository;
+    private final MemberRepository memberRepository;
+    private final EssayRepository essayRepository;
 
     @Override
     public void storeFeedback(Member member, Essay essay, EssayFeedbackInfoDTO essayFeedbackInfoDTO) {
-        EssayFeedback essayFeedback = new EssayFeedback();
         EssayFeedback feedback = EssayFeedback.builder()
                 .member(member)
                 .essay(essay)
-                .content(essayFeedbackInfoDTO.getContent()).build();
+                .content(essayFeedbackInfoDTO.getContent())
+                .build();
         for (int i = 0; i < essayFeedbackInfoDTO.getEveryComment().size(); i++) { //한 문제씩 탐색
             for (EssayFeedbackCommentDTO eachFeedback : essayFeedbackInfoDTO.getEveryComment().get(i).getCommentList()) { //각 문제의 첨삭을 하나씩 탐색
                 EssayFeedbackComment essayFeedbackComment = EssayFeedbackComment.builder() //새로운 첨삭 객체 생성
                         .num(i + 1)
                         .start(eachFeedback.getStart())
                         .end(eachFeedback.getEnd())
-                        .content(eachFeedback.getComment()).build();
-                essayFeedback.getFeedbackComments().add(essayFeedbackComment); //첨삭 리스트에 추가
+                        .content(eachFeedback.getComment())
+                        .essayFeedback(feedback)
+                        .build();
+                feedback.getFeedbackComments().add(essayFeedbackComment); //첨삭 리스트에 추가
             }
         }
-        essayFeedbackRepository.save(essayFeedback);
+        essayFeedbackRepository.save(feedback);
+    }
+
+    @Override
+    public EssayFeedback findFeedback(String nickname, Long essayId) {
+        Member member = memberRepository.findByNickname(nickname);
+        Essay essay = essayRepository.findById(essayId).orElseThrow(
+                ()-> new IllegalArgumentException("자소서 없어요")
+        );
+        return essayFeedbackRepository.findFeedbackByNicknameAndEssay(member, essay);
     }
 
 }
