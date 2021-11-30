983d79aba1cdfdedd46d2c6a6a3b5dde4eaee7f7
SumanKim <kimsm9822@gmail.com>
2021-11-30 17:28:30 +0900

    Feat: admin 페이지 version 1

--- src/main/java/com/SpringIsComing/injagang/Service/TemplateService.java
+++ src/main/java/com/SpringIsComing/injagang/Service/TemplateService.java
@@ -1,6 +1,7 @@
 package com.SpringIsComing.injagang.Service;
 
 import com.SpringIsComing.injagang.DTO.TemplateDTO;
+import com.SpringIsComing.injagang.DTO.TemplateViewDTO;
 import com.SpringIsComing.injagang.Entity.EssayTemplate;
 import com.SpringIsComing.injagang.Entity.EssayTemplateContent;
 import com.SpringIsComing.injagang.Repository.TemplateRepository;
@@ -10,7 +11,8 @@ import java.util.List;
 
 public interface TemplateService {
 
+    List<TemplateViewDTO> findTemplates();
     public void storeEssayTemplate(EssayTemplate essayTemplate);
     public TemplateDTO readTemplate();
-
+    public TemplateViewDTO findTemplate(Long id);
 }
