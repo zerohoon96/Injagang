c37ab1acc79873129cb29deec3b96633630435be
HwangJaeHwan <zxcv0069@naver.com>
2021-11-30 23:21:32 +0900

    Feat : admin 질문추가, 메인페이지 경로 추가

--- src/main/java/com/SpringIsComing/injagang/Entity/QuestionType.java
+++ src/main/java/com/SpringIsComing/injagang/Entity/QuestionType.java
@@ -2,6 +2,13 @@ package com.SpringIsComing.injagang.Entity;
 
 public enum QuestionType {
 
-    CS, SITUATION, JOB, PERSONALITY
+    CS("컴퓨터과학"), SITUATION("상황"), JOB("직무"), PERSONALITY("인성");
 
+    private final String description;
+
+    QuestionType(String description){
+        this.description = description;}
+
+    public String getDescription() {
+        return description;}
 }
