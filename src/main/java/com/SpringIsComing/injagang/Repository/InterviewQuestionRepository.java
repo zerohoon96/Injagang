3b9c9eaef6b308f448973e631a145004e4ba83a4
SumanKim <kimsm9822@gmail.com>
2021-12-01 13:24:09 +0900

    Feat: 면접보기 - 기본 문제 카테고리 추가

--- src/main/java/com/SpringIsComing/injagang/Repository/InterviewQuestionRepository.java
+++ src/main/java/com/SpringIsComing/injagang/Repository/InterviewQuestionRepository.java
@@ -1,10 +1,11 @@
 package com.SpringIsComing.injagang.Repository;
 
 import com.SpringIsComing.injagang.Entity.InterviewQuestion;
+import com.SpringIsComing.injagang.Entity.QuestionType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface InterviewQuestionRepository extends JpaRepository<InterviewQuestion,Long> {
 
-
+    int countByQuestionType(Enum<QuestionType> type);
 
 }
